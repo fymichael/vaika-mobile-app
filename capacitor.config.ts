fde2a9bff32381bcb0750bb229154ff0659756d4
fymichael <118510462+fymichael@users.noreply.github.com>
2024-01-24 10:32:37 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'vaika',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
