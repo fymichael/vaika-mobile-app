e8d2855964327d55d762e87bcf2f8a13bf842338
fymichael <118510462+fymichael@users.noreply.github.com>
2024-02-06 20:45:29 +0300

    part one

--- src/components/authentification/Login.tsx
+++ src/components/authentification/Login.tsx
@@ -1,18 +1,61 @@
 import React, { useState } from 'react';
 import { IonContent, IonInput, IonButton, IonItem, IonLabel, IonPage, IonList, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, useIonRouter, IonLoading } from '@ionic/react';
+import axios from 'axios';
 
-function Login(){
+function stockTokens(key: string, value: string): void {
+    try {
+        localStorage.setItem(key, value);
+    } catch (error) {
+        console.error('Erreur lors du stockage dans le localStorage :', error);
+    }
+}
+
+// fonction pour avoir le tokens du client connecter
+async function getTokens(url: string, data: any): Promise<{ refreshToken: string, accessToken: string } | null> {
+    try {
+        const response = await axios.post(url, data);
+        console.log(response);
+        const { refreshToken, accessToken } = response.data;
+        return { refreshToken, accessToken };
+    } catch (error) {
+        console.error('Erreur lors de la requête POST :', error);
+        return null;
+    }
+}
+
+
+function Login() {
     const navigation = useIonRouter();
-
     const [showLoading, setShowLoading] = useState(false);
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
     const connect = () => {
         setShowLoading(true);
 
+        const data = {
+            username: username,
+            password: password
+        };
+        const url = 'http://localhost:1970/api/voiture/login';
+
         setTimeout(() => {
             setShowLoading(false);
 
-            navigation.push('/annonces');
+            getTokens(url, data)
+                .then((tokens: { refreshToken: string, accessToken: string } | null) => {
+                    if (tokens) {
+                        const { refreshToken, accessToken } = tokens;
+                        stockTokens("refresh-token", refreshToken);
+                        stockTokens("access-token", accessToken);
+                        navigation.push('/annonces');
+                    } else {
+                        alert('token non recuperer');
+                    }
+                })
+                .catch((error: any) => {
+                    console.error('Erreur lors de la requête POST :', error);
+                });
         }, 3000);
     };
 
@@ -28,11 +71,11 @@ function Login(){
                         <IonList>
                             <IonItem className='ion-margin-top'>
                                 <IonLabel position="floating"> Nom d'utilisateur ou email </IonLabel>
-                                <IonInput type="text" className="p_input" />
+                                <IonInput type="text" className="p_input" value={username} onIonChange={(e) => setUsername(e.detail.value!)} />
                             </IonItem>
                             <IonItem className='ion-margin-top'>
                                 <IonLabel position="floating"> Mot de passe </IonLabel>
-                                <IonInput type="password" className="p_input"/>
+                                <IonInput type="password" className="p_input" value={password} onIonChange={(e) => setPassword(e.detail.value!)} />
                             </IonItem>
                             <div className="text-center ion-margin-top">
                                 <IonButton expand="full" className="enter-btn" onClick={() => connect()}> se connecter </IonButton>
